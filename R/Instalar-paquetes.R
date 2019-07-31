f23d077050118b9ae5592e940b37d933c85c3d0a
Favio Vazquez <favio.vazquezp@gmail.com>
2019-07-31 14:02:09 -0700

    Add R files

---
+++ R/Instalar-paquetes.R
@@ -0,0 +1,12 @@
+install.packages(
+  c(
+    "ggplot2", "datos", "rmarkdown", "rprojroot", "evaluate",
+    "digest", "highr", "markdown",   "stringr", "yaml",
+    "Rcpp", "htmltools", "knitr", "jsonlite", "base64enc",
+    "mime", "tidyverse", "tidymodels", "fs", "skimr",
+    "corrr", "babynames", "fueleconomy", "nycflights13",
+    "nasaweather", "Lahman", "gapminder", "xaringan",
+    "flexdashboard", "leaflet", "janitor", "tidytext", "fs",
+    "tidymodels"
+  )
+)
